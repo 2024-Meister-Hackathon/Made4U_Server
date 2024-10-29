78eb12ba8ff42a1717f666fe4be0f3137bece0a1
오송주 <dhthdwn7920@gmail.com>
2024-10-29 21:08:20 +0900

    setting: project setting

---
+++ src/main/java/org/example/made4u/Made4UApplication.java
@@ -0,0 +1,13 @@
+package org.example.made4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Made4UApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Made4UApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/made4u/Made4UApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.made4u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Made4UApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
